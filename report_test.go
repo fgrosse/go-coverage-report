53056101dfb204ccdcc4c5feb81f68039f5faf54
Friedrich Große <friedrich.grosse@gmail.com>
2024-03-16 10:09:09 +0100

    Fix unit tests

--- report_test.go
+++ report_test.go
@@ -26,6 +26,20 @@ func TestReport_Markdown(t *testing.T) {
 |-------------------|------------|---------|
 | github.com/fgrosse/prioqueue | 90.20% (**-9.80%**) | :thumbsdown: |
 | github.com/fgrosse/prioqueue/foo/bar | 0.00% (ø) |  |
-`
+
+---
+
+<details>
+
+<summary>Coverage by file</summary>
+
+| Changed File | Coverage Δ | Total | Covered | Missed | :robot: |
+|--------------|------------|-------|---------|--------|---------|
+| github.com/fgrosse/prioqueue/foo/bar/baz.go | 0.00% (ø) | 0 | 0 | 0 |  |
+| github.com/fgrosse/prioqueue/min_heap.go | 80.77% (**-19.23%**) | 52 (+2) | 42 (-8) | 10 (+10) | :skull:  |
+
+_Please note that the "Total", "Covered", and "Missed" counts above refer to ***code statements*** instead of lines of code. The value in brackets refers to the test coverage of that file in the old version of the code._
+
+</details>`
 	assert.Equal(t, expected, actual)
 }

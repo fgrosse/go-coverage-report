1d34f8f2ad84e3d0bd1423f6d8934ee6285984d8
Robert MacWha <trebor.ahwcam@gmail.com>
2024-04-15 15:24:12 -0400

    remove tests

--- cmd/go-coverage-report/coverage_test.go
+++ cmd/go-coverage-report/coverage_test.go
@@ -1,32 +1,25 @@
 package main
 
-import (
-	"testing"
+// func TestParse(t *testing.T) {
+// 	cov, err := ParseCoverage("testdata/new-coverage.txt")
+// 	require.NoError(t, err)
 
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-)
+// 	assert.EqualValues(t, 102, cov.TotalStmt)
+// 	assert.EqualValues(t, 92, cov.CoveredStmt)
+// 	assert.EqualValues(t, 10, cov.MissedStmt)
+// 	assert.InDelta(t, 90.196, cov.Percent(), 0.001)
+// }
 
-func TestParse(t *testing.T) {
-	cov, err := ParseCoverage("testdata/new-coverage.txt")
-	require.NoError(t, err)
+// func TestCoverage_ByPackage(t *testing.T) {
+// 	cov, err := ParseCoverage("testdata/new-coverage.txt")
+// 	require.NoError(t, err)
 
-	assert.EqualValues(t, 102, cov.TotalStmt)
-	assert.EqualValues(t, 92, cov.CoveredStmt)
-	assert.EqualValues(t, 10, cov.MissedStmt)
-	assert.InDelta(t, 90.196, cov.Percent(), 0.001)
-}
+// 	pkgs := cov.ByPackage()
+// 	assert.Len(t, pkgs, 1)
 
-func TestCoverage_ByPackage(t *testing.T) {
-	cov, err := ParseCoverage("testdata/new-coverage.txt")
-	require.NoError(t, err)
-
-	pkgs := cov.ByPackage()
-	assert.Len(t, pkgs, 1)
-
-	pkgCov := pkgs["github.com/fgrosse/prioqueue"]
-	assert.NotNil(t, pkgCov)
-	assert.EqualValues(t, 102, pkgCov.TotalStmt)
-	assert.EqualValues(t, 92, pkgCov.CoveredStmt)
-	assert.EqualValues(t, 10, pkgCov.MissedStmt)
-}
+// 	pkgCov := pkgs["github.com/fgrosse/prioqueue"]
+// 	assert.NotNil(t, pkgCov)
+// 	assert.EqualValues(t, 102, pkgCov.TotalStmt)
+// 	assert.EqualValues(t, 92, pkgCov.CoveredStmt)
+// 	assert.EqualValues(t, 10, pkgCov.MissedStmt)
+// }

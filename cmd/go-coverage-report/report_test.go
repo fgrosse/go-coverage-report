54fd336eff396639f21ecda12fa30da0fdab3c68
Mikolaj Figurski <miko.f80@gmail.com>
2026-07-30 19:43:42 -0400

    test: no cov change package gets omitted

--- cmd/go-coverage-report/report_test.go
+++ cmd/go-coverage-report/report_test.go
@@ -26,7 +26,6 @@ func TestReport_Markdown(t *testing.T) {
 | Impacted Packages | Coverage Δ | :robot: |
 |-------------------|------------|---------|
 | github.com/fgrosse/prioqueue | 90.20% (**-9.80%**) | :thumbsdown: |
-| github.com/fgrosse/prioqueue/foo/bar | 0.00% (ø) |  |
 
 ---
 
